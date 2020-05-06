f2c79ed8e402af2a4bab23becb7a806aec644f29
Javad Sharifzadeh-Najafi <59621765+Jsharifz@users.noreply.github.com>
2020-05-06 11:35:42 -0600

    Update

--- projects.js
+++ projects.js
@@ -1,32 +1,46 @@
 module.exports = [
     {
         title: `JS App Using an API`,
-        descrption: 'Star-Wars API fetch and event listener menu bar utilizing JavaScript. API currently unsopported.',
-        deployment: 'https://jsharifz.github.io/projects/project%202/index.html',
-        repo: `https://github.com/Jsharifz/JavaScript-Assignments/tree/master/js-assignment3`
+        descrption:
+            "Star-Wars API fetch and event listener menu bar utilizing JavaScript. API currently unsopported.",
+        deployment:
+            "https://jsharifz.github.io/projects/project%202/index.html",
+        repo: `https://github.com/Jsharifz/JavaScript-Assignments/tree/master/js-assignment3`,
     },
     {
         title: `Sass News Site`,
-        descrption: 'Group Sass extension project to generate a framework mock site.',
-        deployment: 'https://jsharifz.github.io/projects/project%203/home.html',
-        repo: `https://github.com/Vaddeil/sass-group-assignment`
+        descrption:
+            "Group Sass extension project to generate a framework mock site.",
+        deployment: "https://jsharifz.github.io/projects/project%203/home.html",
+        repo: `https://github.com/Vaddeil/sass-group-assignment`,
     },
     {
         title: `Gallery MongoDB Custom Gallery`,
-        descrption: 'Dynamically generated pages using express/ejs with data retrieved from MongoDB; image endpoints are the id of the image.',
-        deployment: 'https://javad-node-assignment-3.herokuapp.com/',
-        repo: `https://github.com/Jsharifz/Node-Assignment-3`
+        descrption:
+            "Dynamically generated pages using express/ejs with data retrieved from MongoDB; image endpoints are the id of the image.",
+        deployment: "https://javad-node-assignment-3.herokuapp.com/",
+        repo: `https://github.com/Jsharifz/Node-Assignment-3`,
     },
     {
         title: `Jokathon: A Jekyll Website`,
-        descrption: 'Using the YAML data and markdown documents, a static website was generated which includes a blog post page and collection. *** This website includes satire and crude or dark humor. ***',
-        deployment: 'https://jokeathon.netlify.com/',
-        repo: `https://github.com/Jsharifz/Jekyll-assn-1`
+        descrption:
+            "Using the YAML data and markdown documents, a static website was generated which includes a blog post page and collection. *** This website includes satire and crude or dark humor. ***",
+        deployment: "https://jokeathon.netlify.com/",
+        repo: `https://github.com/Jsharifz/Jekyll-assn-1`,
     },
     {
         title: `Buluva LED Watch`,
-        descrption: 'Bulova Archive Series Digital LED Computron recreated (in Gold, Steel, and Black) using CSS and JavaScript for the clock. The page is best experienced in the FireFox browser.',
-        deployment: 'https://jsharifz.github.io/projects/watch/bulova-gold.html',
-        repo: `https://github.com/Jsharifz/jsharifz.github.io/tree/master/projects/watch`
-    }
-]
+        descrption:
+            "Bulova Archive Series Digital LED Computron recreated (in Gold, Steel, and Black) using CSS and JavaScript for the clock. The page is best experienced in the FireFox browser.",
+        deployment:
+            "https://jsharifz.github.io/projects/watch/bulova-gold.html",
+        repo: `https://github.com/Jsharifz/jsharifz.github.io/tree/master/projects/watch`,
+    },
+    {
+        title: `Pi Estimator`,
+        descrption:
+            "Using random numbers and basic geometry, the value of Pi is estimated with more iterations.",
+        deployment: "https://jsharifz.github.io/projects/pi/index.html",
+        repo: `https://github.com/Jsharifz/Pi-Estimator`,
+    },
+];
