c524e438ef12c8e85b0069ed295a7c3beb2c4fd6
Javad Sharifzadeh-Najafi <59621765+Jsharifz@users.noreply.github.com>
2020-05-29 23:11:16 -0600

    added shopify

--- stack.js
+++ stack.js
@@ -1,47 +1,52 @@
 module.exports = [
     {
         name: `HTML5`,
-        class: 'icon-alt-1',
-        svg: `html5-brands.svg`
+        class: "icon-alt-1",
+        svg: `html5-brands.svg`,
     },
     {
         name: `CSS3`,
-        class: 'icon-alt-2',
-        svg: `css3-alt-brands.svg`
+        class: "icon-alt-2",
+        svg: `css3-alt-brands.svg`,
     },
     {
         name: `SASS`,
-        class: 'icon-alt-1',
-        svg: `sass-brands.svg`
+        class: "icon-alt-1",
+        svg: `sass-brands.svg`,
     },
     {
         name: `JS`,
-        class: 'icon-alt-2',
-        svg: `js-square-brands.svg`
+        class: "icon-alt-2",
+        svg: `js-square-brands.svg`,
     },
     {
         name: `NODEJS`,
-        class: 'icon-alt-1',
-        svg: `node-js-brands.svg`
+        class: "icon-alt-1",
+        svg: `node-js-brands.svg`,
     },
     {
         name: `NPM`,
-        class: 'icon-alt-2',
-        svg: `npm-brands.svg`
+        class: "icon-alt-2",
+        svg: `npm-brands.svg`,
     },
     {
         name: `ADOBE`,
-        class: 'icon-alt-1',
-        svg: `adobe-brands.svg`
+        class: "icon-alt-1",
+        svg: `adobe-brands.svg`,
     },
     {
         name: `JEKYLL`,
-        class: 'icon-alt-2',
-        svg: `jekyll.svg`
+        class: "icon-alt-2",
+        svg: `jekyll.svg`,
     },
     {
         name: `WORDPRESS`,
-        class: 'icon-alt-1',
-        svg: `wordpress-brands.svg`
-    }
-]
+        class: "icon-alt-1",
+        svg: `wordpress-brands.svg`,
+    },
+    {
+        name: `SHOPIFY`,
+        class: "icon-alt-2",
+        svg: `shopify_glyph.svg`,
+    },
+];
