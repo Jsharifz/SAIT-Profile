25cf6ca1aed87b970657645fd9054cad4a5681a3
Javad Sharifzadeh-Najafi <59621765+Jsharifz@users.noreply.github.com>
2020-07-24 20:17:42 -0600

    Update

--- stack.js
+++ stack.js
@@ -25,28 +25,33 @@ module.exports = [
         svg: `node-js-brands.svg`,
     },
     {
-        name: `NPM`,
+        name: `VUEJS`,
         class: "icon-alt-2",
+        svg: `Vue.js_Logo_2.svg`,
+    },
+    {
+        name: `NPM`,
+        class: "icon-alt-1",
         svg: `npm-brands.svg`,
     },
     {
         name: `ADOBE`,
-        class: "icon-alt-1",
+        class: "icon-alt-2",
         svg: `adobe-brands.svg`,
     },
     {
         name: `JEKYLL`,
-        class: "icon-alt-2",
+        class: "icon-alt-1",
         svg: `jekyll.svg`,
     },
     {
         name: `WORDPRESS`,
-        class: "icon-alt-1",
+        class: "icon-alt-2",
         svg: `wordpress-brands.svg`,
     },
     {
         name: `SHOPIFY`,
-        class: "icon-alt-2",
+        class: "icon-alt-1",
         svg: `shopify_glyph.svg`,
     },
 ];
